470c7e63e53aba13e5361a7b74463c41e4c52c93
SriRekha-dev <rekha77468@gmail.com>
2026-08-31 14:55:05 +0530

    Create day2_calculator.py

---
+++ day2_calculator.py
@@ -0,0 +1,11 @@
+# Day 2 - Calculator by Sri Rekha
+print("My Calculator - Made by Sri Rekha")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("You are a calculator pro!")
