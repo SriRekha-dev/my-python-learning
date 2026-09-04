57faea4ea9b18992ec1800c514f2db3a3a81113a
SriRekha-dev <rekha77468@gmail.com>
2026-09-04 13:46:02 +0530

    Update day6_calculator.py

--- day6_calculator.py
+++ day6_calculator.py
@@ -2,6 +2,6 @@ num1 = 10
 num2 = 5
 
 print(f"Addition: {num1 + num2}")
-print(f"Subraction: {num1 - num2}")
+print(f"Subtraction: {num1 - num2}")
 print(f"multiplication: {num1 * num2}")
 print(f"division: {num1 / num2}")
